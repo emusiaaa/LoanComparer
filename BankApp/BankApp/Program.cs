01f7d2d173ca42d50a613a93c2730f8a91ca974e
Agat2oo1 <115614042+Agat2oo1@users.noreply.github.com>
2022-11-22 23:12:17 +0100

    react bank

--- BankApp/BankApp/Program.cs
+++ BankApp/BankApp/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+
 using BankApp.Data;
 using BankApp.Repositories;
 using Microsoft.AspNetCore.Identity;
@@ -16,6 +21,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<LoansComparerDBContext>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+// Make sure a JS engine is registered, or you will get an error!
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+  .AddV8();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -33,6 +44,24 @@ else
 }
 
 app.UseHttpsRedirection();
+app.UseReact(config =>
+{
+    // If you want to use server-side rendering of React components,
+    // add all the necessary JavaScript files here. This includes
+    // your components as well as all of their dependencies.
+    // See http://reactjs.net/ for more information. Example:
+    //config
+    //  .AddScript("~/js/First.jsx")
+    //  .AddScript("~/js/Second.jsx");
+
+    // If you use an external build too (for example, Babel, Webpack,
+    // Browserify or Gulp), you can improve performance by disabling
+    // ReactJS.NET's version of Babel and loading the pre-transpiled
+    // scripts. Example:
+    //config
+    //  .SetLoadBabel(false)
+    //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+});
 app.UseStaticFiles();
 
 app.UseRouting();
