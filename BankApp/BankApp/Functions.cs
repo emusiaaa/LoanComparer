eb77bbc591a47dbef873252fed4d43efaf0bb520
Agat2oo1 <115614042+Agat2oo1@users.noreply.github.com>
2022-12-08 00:34:11 +0100

    Na razie działa i jest id oferty

--- BankApp/BankApp/Functions.cs
+++ BankApp/BankApp/Functions.cs
@@ -1,20 +1,22 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Newtonsoft.Json.Linq;
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
 
-//namespace BankApp
-//{
-//    static public class Functions
-//    {
-//        static public async void WaitingForOffer(HttpClient client, int id)
-//        {
-//            while (true)
-//            {
-//                var result = await client.GetAsync("/api/v1/Inquire/" + $"/{id}");
-//                var resultContent = await result.Content.ReadAsStringAsync();
-//                var status = JObject.Parse(resultContent)["statusDescription"];
-//                //if(status == "OfferPrepared")
-//                Thread.Sleep(1000);
-//            }
-//        }
-//    }
-//}
+namespace BankApp
+{
+    static public class Functions
+    {
+        static public void WaitingForOffer(Object obj)
+        {
+            HttpClient client = (HttpClient)obj;
+            while (true)
+            {
+                //var result = await client.GetAsync("/api/v1/Inquire/38");
+                //var resultContent = await result.Content.ReadAsStringAsync();
+                //var status = JObject.Parse(resultContent)["statusDescription"].ToString();
+                //if (status == "OfferPrepared") break;
+                Thread.Sleep(1000);
+                break;
+            }
+        }
+    }
+}
