01b2c617bb0ab2c904cadb4ab3c25e8bb9847a9c
emusiaaa <emmo.lev@gmail.com>
2022-12-07 19:53:06 +0100

    migracja

--- BankApp/BankApp/Data/Migrations/20221207185037_Update-table-structure.Designer.cs
+++ BankApp/BankApp/Data/Migrations/20221207185037_Update-table-structure.Designer.cs
@@ -4,6 +4,7 @@ using BankApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BankApp.Migrations
 {
     [DbContext(typeof(LoansComparerDBContext))]
-    partial class LoansComparerDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221207185037_Update-table-structure")]
+    partial class Updatetablestructure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
