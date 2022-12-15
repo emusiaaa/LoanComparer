754685c0338d040fec2881b96ccbabd22b411dd8
Anna Rutkiewicz <01161522@pw.edu.pl>
2022-12-15 14:50:49 +0100

    add migration

---
+++ BankApp/BankApp/Data/Migrations/20221215134936_adding-offers-and-offers-summary-tables.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingoffersandofferssummarytables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Percentage = table.Column<double>(type: "float", nullable: false),
+                    MonthlyInstallment = table.Column<double>(type: "float", nullable: false),
+                    RequestedValue = table.Column<double>(type: "float", nullable: false),
+                    RequestedPeriodInMonth = table.Column<int>(type: "int", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    StatusDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InquireId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdateDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApprovedBy = table.Column<int>(type: "int", nullable: true),
+                    DocumentLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DocumentLinkValidDate = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OffersSummary",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InquiryIdInOurDb = table.Column<int>(type: "int", nullable: false),
+                    IsNRInquiry = table.Column<bool>(type: "bit", nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OfferIdInOurDb = table.Column<int>(type: "int", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OffersSummary", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offers");
+
+            migrationBuilder.DropTable(
+                name: "OffersSummary");
+        }
+    }
+}
